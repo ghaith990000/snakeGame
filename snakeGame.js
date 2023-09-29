53245dc2f1cb34f41cdea1aa8c7429d69a2a1350
Ghaith <ghaith990000@gmail.com>
2023-09-29 16:07:14 +0300

    add the render method for the game

--- snakeGame.js
+++ snakeGame.js
@@ -68,3 +68,101 @@ function isGameOver(){
     }
     return false;
 }
+
+// Check if the snake has collided with itself
+function isSnakeCollision(){
+    const head= snake[0];
+    for(let i=1; i<snake.length; i++){
+        if(snake[i].x == head.x && snake[i].y === head.y){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Check if the snake has eaten the food
+function isFoodEaten(){
+    const head = snake[0];
+    if(head.x === food.x && head.y === food.y){
+        return true;
+    }
+    return false;
+}
+
+// Handle food eaten event
+function handleFoodEaten(){
+    snake.push({});
+    food = getRandomFoodPosition();
+    score++;
+    if(score % 5 === 0){
+        increaseSpeed();
+    }
+}
+
+// Increase the game speed
+function increaseSpeed(){
+    clearInterval(gameLoop);
+    speed *= 0.9;
+    gameLoop = setInterval(gameLogic, speed);
+}
+
+// Update the game board
+function renderGame(){
+    const gameBoard = document.getElementById("game-board");
+
+    gameBoard.innerHTML = "";
+
+    // Render snake
+    snake.forEach(segment => {
+        const snakeElement= document.createElement("div");
+        snakeElement.style.gridColumnStart = segment.x + 1;
+        snakeElement.style.gridRowStart = segment.y + 1;
+        snakeElement.classList.add("snake");
+        gameBoard.appendChild(snakeElement);
+    });
+
+    // Render food
+    const foodElement = document.createElement("div");
+    foodElement.style.gridColumnStart = food.x + 1;
+    foodElement.style.gridRowStart = food.y + 1;
+    foodElement.classList.add("food");
+    gameBoard.appendChild(foodElement);
+}
+
+//  Handle keyboard input
+function handleKeyPress(event){
+    switch(event.key){
+        case "ArrowUp":
+            if(direction !== "down"){
+                direction = "up";
+            }
+            break;
+        case "ArrowDown":
+            if(direction !== "up"){
+                direction = "down";
+            }
+            break;
+        case "ArrowLeft":
+            if(direction !== "right"){
+                direction = "left";
+            }
+            break;
+        case "ArrowRight":
+            if(direction !== "left"){
+                direction = "right";
+            }
+            break;
+    }
+}
+
+// Initialize the game
+function init(){
+    // Handle keyboard input
+    document.addEventListener('keydown', handleKeyPress);
+
+    // start the game loop
+    gameLoop = setInterval(gameLogic, speed);
+}
+
+// Start the game
+init();
